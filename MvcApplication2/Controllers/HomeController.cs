4efe60573d0353e30abb5f9df7f96e4286fd9c4d
Popsikels <sigmund.diaz@hotmail.com>
2014-08-30 12:05:42 +0800

    AllRice
    Is good

---
+++ MvcApplication2/Controllers/HomeController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcApplication2.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            ViewBag.Message="Feautured News";
+            return View();
+        }
+
+        public ActionResult Kooperatiba()
+        {
+            return View();
+        }
+
+        public ActionResult Fertilizer()
+        {
+            return View();
+        }
+
+        public ActionResult Presyo()
+        {
+            return View();
+        }
+
+        public ActionResult LocatorMap()
+        {
+            return View();
+        }
+
+        public ActionResult ExtraRice()
+        {
+            return View();
+        }
+    }
+}
